a2f629724f30d4dc90aa969f7f131c857aeecac9
fadeeaal <rakha.fadil@ui.ac.id>
2023-09-16 16:19:48 +0700

    Remove all files

--- main/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from main.models import Product
-
-class ProductForm(ModelForm):
-    class Meta:
-        model = Product
-        fields = ["name", "price", "description"]
