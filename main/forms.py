fd4649bd2c08014efbcedf819561515af11f64fc
fadeeaal <rakha.fadil@ui.ac.id>
2023-09-16 21:27:34 +0700

    rebuild because error

--- main/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from main.models import Item
-
-class ProductForm(ModelForm):
-    class Meta:
-        model = Item
-        fields = ["name", "categories", "description", "price", "amount"]
