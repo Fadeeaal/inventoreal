202087019c2f1e8e325a9da60cdb88f93ea9a95e
fadeeaal <rakha.fadil@ui.ac.id>
2023-09-16 21:46:19 +0700

    big update

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from main.models import Item
 
-class ProductForm(ModelForm):
+class ItemForm(ModelForm):
     class Meta:
         model = Item
         fields = ["name", "categories", "description", "price", "amount"]
